4720ea0c7ca15d55ce781f86b81ab17241571584
Drew Lee <skipmaple@gmail.com>
2024-11-19 00:11:11 +0800

    feat: Add Template project.

---
+++ Template/src/main.c
@@ -0,0 +1,22 @@
+#include "stm32f4xx_hal.h"
+
+/*
+  Global Variables
+*/
+
+/*
+  functions prototypes  (Only declarations are required)
+*/
+
+
+int main() {
+  /*
+    initialize HAL Library. This step is mandatory
+    for HAL libraries to work properly. It configures
+    SysTick etc. for its internal configurations.
+  */
+  HAL_Init();
+  
+  while (1) {
+  }
+}
